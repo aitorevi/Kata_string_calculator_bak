390b0223c0ad66cf48c7239324b5bb5a10f3a6e7
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-03-17 22:57:48 +0100

    First commit to finish point one

---
+++ src/main/java/org/example/KataStringCalculator.java
@@ -0,0 +1,26 @@
+package org.example;
+
+/*
+Punto 1:
+"" -> 0 *
+"1" -> 1
+"2,2" -> 4
+*/
+
+import java.util.List;
+
+public class KataStringCalculator {
+
+    public static int Add(List<String> numbers) {
+        int resultSum = 0;
+        for (String value : numbers) {
+            if (!value.equals("")) {
+                resultSum = resultSum + Integer.parseInt(value);
+            }
+            if (value.equals("")) {
+                return 0;
+            }
+        }
+        return resultSum;
+    }
+}
---
+++ src/test/java/org/example/KataStringCalculatorTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KataStringCalculatorTest {
+
+    @Test
+    void be_zero() {
+        assertEquals(0, KataStringCalculator.Add(List.of("")));
+    }
+
+    @Test
+    void be_one() {
+        assertEquals(1, KataStringCalculator.Add(List.of("1")));
+    }
+
+    @Test
+    void be_result_of_calculator() {
+        assertEquals(4, KataStringCalculator.Add(List.of("2", "2")));
+    }
+}
