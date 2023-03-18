b726157de5dd1f211f7d35fd0e954c10df918257
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-03-18 23:37:20 +0100

    Finish point 2

--- src/main/java/org/example/KataStringCalculator.java
+++ src/main/java/org/example/KataStringCalculator.java
@@ -5,16 +5,21 @@ Punto 1:
 "" -> 0 *
 "1" -> 1 *
 "2,2" -> 4 *
+Punto 2
+"2,2,2" -> 6 *
+Punto 3
+"1\n2,3" -> 6
 */
 
 import java.util.List;
 
 public class KataStringCalculator {
 
-    public static int Add(List<String> numbers) {
+    public static int Add(String text) {
+        var arrayNumbers = text.split(",");
         int resultSum = 0;
-        for (String value : numbers) {
-            if (!value.equals("")) {
+        for (String value : arrayNumbers) {
+            if (!value.equals("") && (!value.equals("\n"))) {
                 resultSum = resultSum + Integer.parseInt(value);
             }
             if (value.equals("")) {
--- src/test/java/org/example/KataStringCalculatorTest.java
+++ src/test/java/org/example/KataStringCalculatorTest.java
@@ -10,16 +10,23 @@ class KataStringCalculatorTest {
 
     @Test
     void be_zero() {
-        assertEquals(0, KataStringCalculator.Add(List.of("")));
+        assertEquals(0, KataStringCalculator.Add(""));
     }
 
     @Test
     void be_one() {
-        assertEquals(1, KataStringCalculator.Add(List.of("1")));
+        assertEquals(1, KataStringCalculator.Add("1"));
     }
 
     @Test
     void be_result_of_calculator() {
-        assertEquals(6, KataStringCalculator.Add(List.of("2", "2", "2")));
+        assertEquals(6, KataStringCalculator.Add("2,2,2"));
     }
+/*
+    @Test
+    void be_result_of_calculator_to_handle_new_lines_between_numbers() {
+        assertEquals(6,KataStringCalculator.Add(List.of("1\n2,3")));
+    }
+
+*/
 }
